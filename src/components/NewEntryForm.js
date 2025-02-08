2bbb8888d2487174e937d00405c342c5f395a690
krishna kireeti <kirikom94@gmail.com>
2025-02-08 11:41:02 +0530

    Refactor all components

---
+++ src/components/NewEntryForm.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Form } from 'semantic-ui-react'
+import ButtonSaveOrCancel from './ButtonSaveOrCancel'
+function NewEntryForm() {
+    return (
+        <Form unstackable>
+            <Form.Group>
+                <Form.Input icon="tags" width={12} label="Description" placeholder="New Shinny thing" />
+                <Form.Input width={4} label='value' placeholder="100.00" icon='dollar' iconPosition='left'></Form.Input>
+            </Form.Group>
+            <ButtonSaveOrCancel />
+        </Form>
+    )
+}
+
+export default NewEntryForm
